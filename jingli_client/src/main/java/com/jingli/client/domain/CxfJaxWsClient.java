a3493e75815c4f1687746e15ec00579fa42f1cc6
卢阿龙 <along.lu@jingli365.com>
2019-04-21 22:52:39 +0800

    本地文件tijiao

--- jingli_client/src/main/java/com/jingli/client/domain/CxfJaxWsClient.java
+++ jingli_client/src/main/java/com/jingli/client/domain/CxfJaxWsClient.java
@@ -12,6 +12,7 @@ public class CxfJaxWsClient {
 	public static void main(String[] args) {
 		
 		//webservice
+		//546456575645646
 		JaxWsProxyFactoryBean proxyFactory=new JaxWsProxyFactoryBean();
 		
 		proxyFactory.setAddress("http://localhost:8080/jingli-elutong/services/updateInterface");
